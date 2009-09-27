126d5e4d66a66ae545243692ef6caac2cc42bbdb
Marco Louro <mlouro@gmail.com>
2009-09-27 19:22:41 +0100

    changed passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,10 @@
 import os, sys
 import site
 
-site.addsitedir('/opt/webapps/viriato.reactivelab.com/lib/python2.5/site-packages')
+site.addsitedir('/opt/webapps/viriato.reactivelab.com/lib/python2.6/site-packages')
+site.addsitedir('/opt/webapps/viriato.reactivelab.com/src/viriato-trunk')
+site.addsitedir('/opt/webapps/viriato.reactivelab.com/src/viriato-trunk/viriato')
 
-
-#automatically finds application's current path
-nginx_configuration= os.path.dirname(__file__)
-project = os.path.dirname(nginx_configuration)
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'viriato.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-
